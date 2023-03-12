7cfbcd2fd53385106a210d643cd530448e7beeea
acwell94 <acwell94@naver.com>
2023-03-13 00:56:03 +0900

    230313 공부내용 기능테스트 상호작용 테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
-
-test("button has correct initial color", () => {
-  render(<App />);
+import { logRoles } from "@testing-library/react";
+test("button has correct initial color, and update when Clicked", () => {
+  const { container } = render(<App />);
+  logRoles(container);
   // 버튼 초기 여부
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
   // 백그라운드 레드로 변경
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // Click Button
+  fireEvent.click(colorButton);
+
+  // colorButton backGround Change
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // colorButton text Change
+  expect(colorButton).toHaveTextContent("Change to red");
 });
 
-test("button turn blue when clicked", () => {});
+// test("button turn blue when clicked", () => {
+//   render(<App/>)
+//   const colorButton = screen.getByRole('button', {name: "Change to blue"})
+// });
